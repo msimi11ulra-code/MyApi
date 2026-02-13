1a5d1acb24be882cf005a52e53ac508a713da4fe
msimi11ulra-code <msimi11ulra@gmail.com>
2026-02-14 01:56:47 +0200

    Add files via upload

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,9 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace MyApi.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0, 1000000)]
         public decimal Price { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? Category { get; set; }
     }
 }
