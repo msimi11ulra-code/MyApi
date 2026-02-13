98b80684ab82b1864f2b9428cdf3b3f894b2f26a
msimi11ulra-code <msimi11ulra@gmail.com>
2026-02-13 17:14:18 +0200

    initial commit

---
+++ MyApi/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApi.Models;
+
+namespace MyApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
