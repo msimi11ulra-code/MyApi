80ce739a339f49288433d99aaa9269fb4bcae346
msimi11ulra-code <msimi11ulra@gmail.com>
2026-02-14 19:57:23 +0200

    Fix Program.cs and enable authentication

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 // 3. Configure Entity Framework Core with SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // 4. Configure CORS
 builder.Services.AddCors(options =>
